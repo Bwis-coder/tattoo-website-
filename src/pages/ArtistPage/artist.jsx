14760a2059ce4d57ce0fc3c20ef4cc0a6f2c548e
Bwis <wisdomEzekiel9@gmail.com>
2026-06-20 13:53:21 +0100

    Add lazy loading and explicit dimensions to artist images

--- src/pages/ArtistPage/artist.jsx
+++ src/pages/ArtistPage/artist.jsx
@@ -1,10 +1,10 @@
 import { artists } from "../../utilty/aritist.js";
 import "./artist.css";
-import { Header } from "../Header/header.jsx"
+import { Header } from "../Header/header.jsx";
 const ArtistPage = () => {
   return (
     <div className="artist-container">
-      <Header/>
+      <Header />
       <h1>Meet Our Artists</h1>
       <h3>
         Our talented team of tattoo artists brings creativity, experience, and
@@ -12,7 +12,7 @@ const ArtistPage = () => {
       </h3>
       {artists.map((artist) => (
         <div className="artist-card" key={artist.id}>
-          <img src={artist.image} alt={artist.name} />
+          <img src={artist.image} alt={artist.name} loading="lazy" />
           <h2>{artist.name}</h2>
           <h3>{artist.years}</h3>
           <p>{artist.specialty}</p>
